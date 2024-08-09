7aeb42976456eacd52d9b6c31844ed0a1b070ca5
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-09 09:39:35 +0200

    making it pep8 styling

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,15 +9,14 @@ def pascal_triangle(n):
     pascal triangle function
     """
     main_array = []
-    if (n <= 0):
+    if n <= 0:
         return []
-    
     for i in range(n):
         if i == 0:
             main_array.append([1])
         elif i == 1:
             main_array.append([1, 1])
-        if (i > 1):
+        if i > 1:
             temp_array = main_array[len(main_array)-1]
             number_array = []
             for j in range(len(temp_array) - 1):
@@ -25,4 +24,5 @@ def pascal_triangle(n):
                     number = temp_array[j] + temp_array[j+1]
                     number_array.append(number)
             main_array.append([1, *number_array, 1])
+            
     return main_array
