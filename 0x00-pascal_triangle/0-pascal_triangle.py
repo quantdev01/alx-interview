8aa1a2a7fe408734d294828c6aa9f5e4d2b30c35
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-09 09:43:25 +0200

    FIxed style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 """
-Function returning a list of lists of integers representing the 
+Function returning a list of lists of integers representing the
 Pascal triangle of n
 """
+
+
 def pascal_triangle(n):
     """
     pascal triangle function
@@ -17,12 +19,12 @@ def pascal_triangle(n):
         elif i == 1:
             main_array.append([1, 1])
         if i > 1:
-            temp_array = main_array[len(main_array)-1]
+            temp_array = main_array[len(main_array) - 1]
             number_array = []
             for j in range(len(temp_array) - 1):
-                if (j != len(main_array) -1):
-                    number = temp_array[j] + temp_array[j+1]
+                if (j != len(main_array) - 1):
+                    number = temp_array[j] + temp_array[j + 1]
                     number_array.append(number)
             main_array.append([1, *number_array, 1])
-            
+
     return main_array
