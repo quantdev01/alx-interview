b48eca93985a843d244a4af715f0f24f556fa423
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-09-06 10:09:17 +0200

    Task 0 utf-8 validation, fixed pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
-     # Number of bytes in the current UTF-8 character
+    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
     # Masks to identify the most significant bits
