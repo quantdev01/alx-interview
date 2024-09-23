06c033c5b5623043811881dafc7398573f7bdedb
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-09-23 23:22:06 +0200

    Task 0 update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,36 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Fetch the film data from the Star Wars API
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, function (err, res, body) {
   if (err) throw err;
+
+  // Parse the film data
   const actors = JSON.parse(body).characters;
+
+  // Function to print characters in exact order
+  const exactOrder = (actors, index) => {
+    if (index === actors.length) return;
+
+    // Fetch the character data
+    request(actors[index], function (err, res, body) {
+      if (err) throw err;
+      
+      // Print the character's name
+      console.log(JSON.parse(body).name);
+
+      // Recursive call to print the next character
+      exactOrder(actors, index + 1);
+    });
+  };
+
+  // Start printing the characters
   exactOrder(actors, 0);
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
+
