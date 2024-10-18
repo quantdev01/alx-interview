82ac02cad74698bbc0a55c010b904cdeef835f60
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-10-18 03:13:56 +0200

    task 0 works now

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,46 +4,39 @@ Module, this will be checking a game
 """
 
 
-def isWinner(x, nums):
-    """
-    description
-    """
-    def sieve_of_eratosthenes(max_n):
-        """
-        sieve sieve_of_eratosthenes
-        """
-        is_prime = [True] * (max_n + 1)
-        p = 2
-        while (p * p <= max_n):
-            if (is_prime[p]):
-                for i in range(p * p, max_n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, max_n + 1) if is_prime[p]]
+def sieve_of_eratosthenes(n):
+    """Generates a list of prime numbers up to n using the Sieve of Eratosthenes."""
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, n + 1, i):
+                primes[j] = False
+    return [i for i in range(2, n + 1) if primes[i]]
 
-    max_n = max(nums)  # Find the maximum value of n from nums
-    primes = sieve_of_eratosthenes(max_n)  # Get all primes up to max_n
+def isWinner(x, nums):
+    """Determines the winner of the most rounds in the Prime Game."""
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+    primes = sieve_of_eratosthenes(max_n)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        prime_set = set(primes)  # Create a set of primes for the current n
-        current_player = 0  # 0 for Maria, 1 for Ben
+        primes_in_game = [p for p in primes if p <= n]
+        moves = 0
+        while primes_in_game:
+            prime = primes_in_game.pop(0)
+            moves += 1
+            primes_in_game = [p for p in primes_in_game if p % prime != 0]
 
-        while prime_set:
-            # Choose the smallest prime
-            chosen_prime = min(prime_set)
-            # Remove the chosen prime and its multiples from the set
-            for multiple in range(chosen_prime, n + 1, chosen_prime):
-                prime_set.discard(multiple)
-            # Switch players
-            current_player = 1 - current_player
-
-        if current_player == 0:  # If current_player is 0, then Ben won
-            ben_wins += 1
-        else:  # If current_player is 1, then Maria won
+        if moves % 2 == 1:
             maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
